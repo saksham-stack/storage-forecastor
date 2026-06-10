1f74ef8f4c4e45f4379629e30f6c62447510a2ca
saksham-stack <sakshamguptaqaz@gmail.com>
2026-06-11 01:31:30 +0530

    debugging backend files and enhancing

--- src/app_reviews.py
+++ src/app_reviews.py
@@ -1,3 +1,3 @@
-from src.review_store import backend_label, healthcheck, load_reviews, review_summary, save_review
+from review_store import backend_label, healthcheck, load_reviews, review_summary, save_review
 
 __all__ = ['backend_label', 'healthcheck', 'load_reviews', 'review_summary', 'save_review']
